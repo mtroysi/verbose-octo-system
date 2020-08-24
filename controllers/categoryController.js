2198a4c0c8c0ac1140c5150faebaa85062b1db13
Morgane TROYSI <troysi.m@sfeir.com>
2020-08-24 16:41:20 +0200

    Category list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,13 @@ var Category = require('../models/category');
 
 // Display list of all Categories.
 exports.category_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Category list');
+    Category.find({})
+    .sort([['name', 'ascending']])
+    .exec(function (err, list_categories) {
+      if (err) { return next(err); }
+      //Successful, so render
+      res.render('category_list', { title: 'Category List', category_list: list_categories });
+    });
 };
 
 // Display detail page for a specific Category.
--- routes/catalog.js
+++ routes/catalog.js
@@ -58,6 +58,6 @@ router.post('/category/:id/update', category_controller.category_update_post);
 router.get('/category/:id', category_controller.category_detail);
 
 // GET request for list of all Categories.
-router.get('/categorys', category_controller.category_list);
+router.get('/categories', category_controller.category_list);
 
 module.exports = router;
