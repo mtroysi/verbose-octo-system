dc4f606acbfe4ed1cc353706dcf3b1f2540082a9
mtroysi <morgane.troysi@posteo.net>
2020-08-25 11:10:48 +0200

    Category detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 var Category = require('../models/category');
+var Item = require('../models/item');
+var async = require('async');
 
 // Display list of all Categories.
 exports.category_list = function(req, res) {
@@ -13,7 +15,25 @@ exports.category_list = function(req, res) {
 
 // Display detail page for a specific Category.
 exports.category_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Category detail: ' + req.params.id);
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id)
+              .exec(callback)
+        },
+        category_items: function(callback) {
+          Item.find({ 'category': req.params.id },'name description')
+          .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.category==null) { // No results.
+            var err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('category_detail', { title: 'Category Detail', category: results.category, category_items: results.category_items } );
+    });
 };
 
 // Display Category create form on GET.
